06cdc227ac0510367712b48989339fc94fab043e
Gamze <gamzeyasar35@icloud.com>
2023-02-10 09:48:22 +0300

    check succes

--- game.js
+++ game.js
@@ -5,12 +5,12 @@
 
 
 // //? 1-100 arasinda rasgele bir sayi tut.
-// let randomNumber = Math.round(Math.random() * 100);
-// console.log(randomNumber);
+let randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
 
 // //? Variables
-// let score = 10;
-// // let topScore = 0;
+let score = 10;
+let topScore = 0;
 
 // //? localStorage'de topScore adiyla bir degisken olustur.
 // let topScore = localStorage.getItem("topScore") || 0;
@@ -18,75 +18,86 @@
 // //? DOM'daki top-score degerini localStorage'den okuyarak guncelle.
 // document.querySelector(".top-score").textContent = topScore;
 
-// //* CheckBtn basildiginda kontrolleri yap
-// document.querySelector(".check-btn").addEventListener("click", () => {
-//     const guessInput = Number(document.querySelector(".guess-input").value);
-//     const msg = document.querySelector(".msg");
-//     const body = document.querySelector("body");
+// //* 2- CheckBtn basildiginda kontrolleri yap
+document.querySelector(".check-btn").addEventListener("click", () => {
+    const guessInput = Number(document.querySelector(".guess-input").value);
+    const msg = document.querySelector(".msg");
+    const body = document.querySelector("body");
 
-//     //? eger input girilmediyse Kullaniciya uyari ver.
-//     if (!guessInput) {
-//         msg.innerText = "Please enter a number";
-//         //! eger rasgele == input.value
-//     } else if (randomNumber === guessInput) {
-//         msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts f
-//     body.className = "bg-success";
-//     document.querySelector(".check-btn").disabled = true;
-//     if (score > topScore) {
-//       // topScore = score;
+    //! DİKKAT!👉 Bu guessInput'u, msg ve body yı dısarda tanımlasaydık global olurdu ve bır sefer tanımlamamız yeterli olurdu.
+    //! Ama burada biz onları localde tanımladık, her defasında bu sefer tanımlamamız gerekiyor.Ama 🎇BELLEK KULLANIMI ACISINDAN LOCAL KULLANMAK DAHA MANTIKLI, yazma konusu acısından global yapmak mantıklı.LOCAL yapmamızın sebebi belleği daha effectif kullanmak için.
+    //TODO 🎇Burada guessInput,msg,body sadece ve sadece check butonuna basıldıgında olusuyor 
 
-//       //? localStorage'deki topScore degiskenini guncelle
-//       localStorage.setItem("topScore", score);
-//       //? DOM'daki top-score degerini guncelle
-//       document.querySelector(".top-score").textContent = score;
-//     }
-//     document.querySelector(".secret-number").textContent = randomNumber;
+    //     //? eger input girilmediyse Kullaniciya uyari ver.
+    if (!guessInput) {
+        msg.innerText = "Please enter a number";
+        //! eger rasgele == input.value
+    } else if (randomNumber === guessInput) {
+        msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
+        body.className = "bg-success";
+        document.querySelector(".check-btn").disabled = true;
+        if (score > topScore) {
+            topScore = score;
+            //!👆kullanıcı topScore a girdiğinde topScore her zaman küçük oldukca true döenecek ve alta gececek, scoreden yansıyacak.
+            //! Bu güncelleme sadece yukarıdaKI VARIEBLES seviyesind e bır güncelleme 
+            //! Ve bir dikkat etmemmız gerekende(score > topScore) {
+            //! topScore = score; bunu yaparak DOM A nir sey yansıtmıyoruz yani top scorede görünen bır değişiklşik olmuyor. görünen bır degısıklık olması için👉 document.querySelector(".top-score").textContent = score; u yazıyoruz.
 
-//     //! eger rasgele!= input.value
-//   } else {
-//     score--;
-//     if (score > 0) {
-//       guessInput > randomNumber
-//         ? (msg.innerHTML = `< i class="fa-solid fa-arrow-trend-down fa-2x" ></i >
-//         : (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> I
-//     } else {
-//       msg.innerHTML = `You Lost < i class="fa-regular fa-face-sad-tear fa-2x" > <
-//             document.querySelector(".secret-number").textContent = randomNumber;
-//         body.className = "bg-danger";
-//         document.querySelector(".check-btn").disabled = true;
-//     }
+            //? localStorage'deki topScore degiskenini guncelle
+            //? yanı kalıcı belleğe atma 
+            localStorage.setItem("topScore", score);
+            //? DOM'daki top-score degerini guncelle
+            document.querySelector(".top-score").textContent = score;
+        }
+        document.querySelector(".secret-number").textContent = randomNumber;
+        //TODO🎇👆 kullanıcı bildiğinde girilen number i görmesi için else-if hızsınsa yazdık
 
-//     document.querySelector(".score").textContent = score;
-// }
-// });
+        //! eger rasgele!= input.value
+    } else {
+        score--;
+        // yada score=-1; diyedebiliriz aynı sey
+        if (score > 0) {
+            guessInput > randomNumber
+                ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-down fa-2x"></i >DECREASE `)
+                : (msg.innerHTML = `<i i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE `);
+        } else {
+            msg.innerHTML = `You Lost < i class="fa-regular fa-face-sad-tear fa-2x" /> </i>`;
+            document.querySelector(".secret-number").textContent = randomNumber;
+            body.className = "bg-black";
+            document.querySelector(".check-btn").disabled = true;
+        }
+
+        document.querySelector(".score").textContent = score;
+    }
+});
 
 // //* again basildiginda oyunu baslangic dgerlerin kur
-// document.querySelector(".again-btn").addEventListener("click", () => {
-//     score = 10;
-//     document.querySelector(".score").textContent = score;
-//     randomNumber = Math.round(Math.random() * 100);
-//     document.querySelector(".secret-number").textContent = "?";
-//     console.log(randomNumber);
-//     document.querySelector(".check-btn").disabled = false;
-//     document.querySelector("body").classList.remove("bg-success", "bg-danger");
-//     document.querySelector(".guess-input").value = "";
-//     document.querySelector(".msg").innerText = `Starting..`;
-// });
+document.querySelector(".again-btn").addEventListener("click", () => {
+    score = 10;
+    document.querySelector(".score").textContent = score;
+    randomNumber = Math.round(Math.random() * 100);
+    document.querySelector(".secret-number").textContent = "?";
+    console.log(randomNumber);
+    document.querySelector(".check-btn").disabled = false;
+    document.querySelector("body").classList.remove("bg-success", "bg-danger");
+    document.querySelector(".guess-input").value = "";
+    document.querySelector(".msg").innerText = `Starting..`;
+});
 
-// document.querySelector(".guess-input").addEventListener("keydown", (e) => {
-//     if (e.code === "Enter") {
-//         document.querySelector(".check-btn").click();
-//     }
-// });
+document.querySelector(".guess-input").addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+        document.querySelector(".check-btn").click();
+    }
+});
 
-// //! LOCALSTORAGE- SESSIONSTORAGE
-// // myObj = { a: 1, b: 2, c: 3 };
-// // localStorage.setItem("OBJ", JSON.stringify(myObj));
-// // const readObj = localStorage.getItem("OBJ");
-// // const readOBJ = JSON.parse(localStorage.getItem("OBJ"));
-// // console.log(typeof readObj);
-// // console.log(typeof readOBJ);
-// // console.log(readOBJ);
+//! LOCALSTORAGE- SESSIONSTORAGE
+myObj = { a: 1, b: 2, c: 3 };
+localStorage.setItem("OBJ", JSON.stringify(myObj));
+const readObj = localStorage.getItem("OBJ");
+const readOBJ = JSON.parse(localStorage.getItem("OBJ"));
+console.log(typeof readObj);
+console.log(typeof readOBJ);
+console.log(readOBJ);
 
 // //* PUSEDUO
 // //? eger score > topScore
